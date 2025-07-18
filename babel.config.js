71a84cdaadedc21ed860a7b68a1d1a5a5afaa00f
Lior S. <79309515+liorshap1@users.noreply.github.com>
2025-07-18 15:53:29 +0300

    Add Babel plugin for logical assignment operators
    Included @babel/plugin-transform-logical-assignment-operators in Babel config and dependencies to support logical assignment operator syntax in the project.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = (api) => {
 	api.cache(true);
 	return {
-		plugins: ["react-native-reanimated/plugin"],
+		plugins: ["react-native-reanimated/plugin", "@babel/plugin-transform-logical-assignment-operators"],
 		presets: ["babel-preset-expo"],
 	};
 };
